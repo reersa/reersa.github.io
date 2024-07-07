4122f5e6f30b1f9c68ec19ee295c893022e11b0c
Aaron Reers <aaronreers@gmail.com>
2024-07-07 17:13:28 -0400

    Header complete

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<script setup>
+import Tabs from 'primevue/tabs';
+import TabList from 'primevue/tablist';
+import Tab from 'primevue/tab';
+import TabPanels from 'primevue/tabpanels';
+import TabPanel from 'primevue/tabpanel';
+import { ref } from 'vue';
+
+const tabs = ref([
+    { title: 'Home', value: '0' },
+    { title: 'Our Story', value: '1' },
+    { title: 'Photos', value: '2' },
+    { title: 'Ceremony Details', value: '3' },
+    { title: 'Travel/Hotel', value: '4' },
+    { title: 'Registry', value: '5' },
+    { title: 'FAQ', value: '6' },
+]);
+const selectedTab = ref(0);
+
+function SelectTab(tab)
+{
+  selectedTab.value = tab.value;
+}
+</script>
+
+<template>
+  <div>
+    <header>
+      <div id="headerBackground"></div>
+      <span id="headerNames">Aaron & Falon</span>
+      <Tabs id="headerTabs" value=0>
+        <TabList>
+            <Tab v-for="tab in tabs" @click="SelectTab(tab);" style="width:100%" :key="tab.title" :value="tab.value">{{ tab.title }}</Tab>
+        </TabList>
+      </Tabs>
+    </header>
+    <main>
+      <div v-if="selectedTab == 0">
+        <span>Home</span>
+      </div>
+      <div v-if="selectedTab == 1">
+        <span>Our Story</span>
+      </div>
+      <div v-if="selectedTab == 2">
+        <span>Photos</span>
+      </div>
+      <div v-if="selectedTab == 3">
+        <span>Ceremony Details</span>
+      </div>
+      <div v-if="selectedTab == 4">
+        <span>Travel/Hotel</span>
+      </div>
+      <div v-if="selectedTab == 5">
+        <span>Registry</span>
+      </div>
+      <div v-if="selectedTab == 6">
+        <span>FAQ</span>
+      </div>
+    </main>
+  </div>
+</template>
